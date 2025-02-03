17c1abe22d3ddd05eb642a3c43f580707e84da1c
Chris Nolan <cnolan@gmail.com>
2025-02-03 22:19:28 +1100

    Add some explanation for the single animal event script

--- acan2025/scripts/explore_single_animal.py
+++ acan2025/scripts/explore_single_animal.py
@@ -1,10 +1,37 @@
+# %% [markdown]
+#
+# # Exploring event data
+#
+# This script is a simple overview of how to load event data from
+# sessions in a single animal.
+
+# %% [markdown]
+#
+# We need to load some libraries to help us work with our data.
+#
+# pandas: A library for working with data in a table format. If you are
+#         familiar with R, pandas is similar to the data.frame object.
+# pathlib: A library for working with file paths.
+# hvplot: A library for creating interactive plots. For static /
+#         print-ready plots, or for more complex faceted data plots, the
+#         seaborn and/or matplotlib libraries might be more appropriate.
+
 # %%
 import pandas as pd
 from pathlib import Path
 import hvplot.pandas
 
+# %% [markdown]
+#
+# We can refer to a path on the filesystem either relative to the
+# current location or absolutely. The path object below is expecting the
+# "rawdata/sub-angelina-ses-RR20.03" folder to be in the parent
+# directory of the current location.
+
 # %%
 data_path = Path('../rawdata/sub-angelina/ses-RR20.03')
+
+# %% [markdown]
 
 # %%
 events_paths = [str(p) for p in data_path.glob('*_events.csv')]
