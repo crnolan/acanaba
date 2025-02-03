cc31bcb21d620db654e98d45f2b4efdaaf66a208
Chris Nolan <cnolan@gmail.com>
2025-02-03 21:14:28 +1100

    More on single animal event analysis

--- acan2025/scripts/explore_single_animal.py
+++ acan2025/scripts/explore_single_animal.py
@@ -1,24 +1,52 @@
 # %%
 import pandas as pd
 from pathlib import Path
+import hvplot.pandas
 
 # %%
 data_path = Path('../rawdata/sub-angelina/ses-RR20.03')
 
 # %%
-events_path = [str(p) for p in data_path.glob('*A_events.csv')][0]
-events_path
+events_paths = [str(p) for p in data_path.glob('*_events.csv')]
+events_paths
 
 # %%
-events = pd.read_csv(events_path)
+events = pd.read_csv(events_paths[0])
 events['onset'] = pd.to_timedelta(events['onset'], unit='s')
-events = events.set_index('onset')
+events = events.fillna(0.01).set_index('onset')
 events
 
-# %%
-events.groupby('event_id').rolling(pd.to_timedelta('60s')).count()
+# %% [markdown]
+#
+# The formatting below is just to make the "method chaining" clear and
+# avoid excessively long lines. One could just write the first
+# assignment as:
+#
+# ```python
+# event_rates = events.groupby('event_id').rolling(pd.to_timedelta('30s')).count().rename(columns={'duration': 'rate'}))
+# ```
+#
+# but it's much harder to read and quite possibly requires horizontal
+# scrolling to read the whole line.
 
 # %%
-events.loc[events['event_id'] == 'lp'].count()
+event_rates = (
+    events
+        .groupby('event_id')
+        .rolling(pd.to_timedelta('30s'))
+        .count()
+        .rename(columns={'duration': 'rate'}))
+event_rates
 
 # %%
+event_rates.hvplot.line(x='onset', y='rate', by='event_id')
+
+# %% [markdown]
+#
+# What if we wanted both acquisition sessions? We could load them into
+# separate variables, or into a list of DataFrames. But we could also
+# just load them into a single dataframe and identify each acquision
+# session with an extra column.
+
+# %%
+
