985f686bafa1e6ee69304004ffaa36581ad91f83
Chris Nolan <cnolan@gmail.com>
2026-05-25 15:56:17 +1000

    Add DLC analysis script

---
+++ acan2026/scripts/analyse_trained.py
@@ -0,0 +1,46 @@
+# %%
+import deeplabcut
+from pathlib import Path
+
+# %%
+project_path = Path('/scratch/acan/acanaba/acan2026')
+analysis_path = project_path / 'derivatives/dlc/analysed_shuffle3'
+dlc_path = Path('/home/cnolan/Projects/medass_topviewmouse-cn-2025-08-07')
+dlc_config = dlc_path / 'config.yaml'
+video_root = project_path / 'rawdata'
+video_glob = '**/*.mp4'
+video_paths = [str(fn)
+               for fn in video_root.glob(video_glob)]
+            #    if not (analysis_path / (fn.stem + 'DLC_HrnetW32_acanJan28shuffle6_detector_360_snapshot_110.h5')).exists()]
+superanimal_name = 'superanimal_topviewmouse'
+model_name = 'hrnet_w32'
+detector_name = 'fasterrcnn_resnet50_fpn_v2'
+
+# %%
+deeplabcut.analyze_videos(
+    dlc_config,
+    videos=video_paths,
+    shuffle=3,
+    device='cuda:0')
+
+# %%
+video_paths = [str(fn)
+               for fn in video_root.glob(video_glob)]
+            #    if not (analysis_path / (fn.stem + 'DLC_HrnetW32_acanJan28shuffle6_detector_360_snapshot_110_filtered.h5')).exists()]
+deeplabcut.filterpredictions(dlc_config,
+                             video_paths,
+                             shuffle=3)
+# deeplabcut.plot_trajectories(config_path, video_paths)
+
+# %%
+video_paths = [str(fn)
+               for fn in video_root.glob(video_glob)]
+            #    if not (analysis_path / (fn.stem + 'DLC_HrnetW32_acanJan28shuffle6_detector_360_snapshot_110_filtered_p60_labeled.mp4')).exists()]
+deeplabcut.create_labeled_video(dlc_config,
+                                video_paths,
+                                shuffle=3,
+                                filtered=True,
+                                save_frames=False,
+                                draw_skeleton=False)
+
+# %%
